88d12be033c7d39c5f5adb8f03de0373db46e55f
Gergely Tóth <thgeri17@gmail.com>
2024-04-21 15:34:48 +0200

    Spring starter project

---
+++ src/test/java/thgeri17/footievault/FootieVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package thgeri17.footievault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootieVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
