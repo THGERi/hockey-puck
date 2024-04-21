48f08ad55adf6d2020b819da6e97494919b6e69e
Gergely Tóth <thgeri17@gmail.com>
2024-04-21 15:36:23 +0200

    Add Enums

---
+++ src/main/java/thgeri17/footievault/value/ErrorCode.java
@@ -0,0 +1,6 @@
+package thgeri17.footievault.value;
+
+public enum ErrorCode {
+
+    NOT_FOUND
+}
---
+++ src/main/java/thgeri17/footievault/value/Role.java
@@ -0,0 +1,8 @@
+package thgeri17.footievault.value;
+
+public enum Role {
+    FORWARD,
+    WINGER,
+    DEFENSMAN,
+    GOALIE
+}
